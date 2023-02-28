bd96ade3378bbb8df794602cc42e525e474b9ed5
SungJungHo1git config --global user.name SungJungHo1 <s214q@naver.com>
2023-02-28 20:28:35 +0900

    sd

--- DBMaker.py
+++ DBMaker.py
@@ -116,7 +116,7 @@ def Insert_cust(UserName, UserId, phone):
     str_datetime = datetime.strftime(datetime_utc2, format)
     str_Days = datetime.strftime(datetime_utc2, format_Days)
 
-    Insert_CouponTime(str_Days,"First Coupon",Coupon_Code,UserName)
+    Insert_CouponTime(str_Days,"First Coupon",Coupon_Code,UserId)
 
     mycustomer.insert_one(
         {"UserName": UserName, 'UserId': UserId, "address1": "", "address2": "", "phone": phone, "memo": "", 'Point': 0,'Re_Point': 0, 'Start_Time': str_datetime,
