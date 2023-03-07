89805a93356403fc85a36d05528d2a6983b8b7c1
SungJungHo1git config --global user.name SungJungHo1 <s214q@naver.com>
2023-03-07 23:33:16 +0900

    sd

--- Get_yogiyo.py
+++ Get_yogiyo.py
@@ -63,7 +63,7 @@ def get_Menu(id):
         'Accept-Encoding': 'gzip, deflate',
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari Line/13.2.1 LIFF',
         'Accept-Language': 'ko-KR,ko;q=0.9',
-        'Referer': 'https://thailovefood.com/menu/311101',
+        'Referer': f'https://thailovefood.com/menu/{id}',
 
         }
 
