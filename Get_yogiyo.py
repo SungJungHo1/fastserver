d4b4d453799d52adb4e8181d44a5f6d643e99de5
SungJungHo1 <s214q@naver.com>
2023-03-15 13:44:32 +0900

    sdsd2

--- Get_yogiyo.py
+++ Get_yogiyo.py
@@ -7,6 +7,7 @@ from Ordersdatas import *
 from Make_Datas import *
 from DBMaker import *
 from AccessToken import *
+import cloudscraper
 import httpx
 
 
@@ -59,45 +60,44 @@ def Upload_IMG(image):
     
 #     return Get_json
 
-def get_Menu(id):
-    header = {   
-        
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari Line/13.2.1 LIFF',
-        'Accept-Language': 'ko-KR,ko;q=0.9',
-        'Referer': f'https://thailovefood.com/menu/{id}',
-        }
-
-    url = f"https://www.thailovefood.com/menu_info/{id}"
-    url2 = f"http://yogiyofind.ddns.net/getMenus?id={id}"
-    try:
-        response = requests.get(url,headers=header,verify=False,timeout=2)
-    except:
-        response = requests.get(url2,headers=header,verify=False)
-    Get_json = response.json()
-
-    return Get_json
-
 # def get_Menu(id):
 #     header = {   
         
-#         'Cookie': 'PHPSESSID=rh3a3euioieoau71nd72s2q76u',
 #         'Accept': '*/*',
 #         'Accept-Encoding': 'gzip, deflate',
 #         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari Line/13.2.1 LIFF',
 #         'Accept-Language': 'ko-KR,ko;q=0.9',
-        
+#         'Referer': f'https://thailovefood.com/menu/{id}',
 #         }
 
-#     url = f"https://youduay.com/datacenter/getajaxfood.php?type=6&sid={id}"
-#     with httpx.Client(headers=header,http2=True) as client:
-
-#         response = client.get(url,headers=header)
-#         print(f"\n\n{response.content}\n\n")
-#         Get_json = response.json()
+#     url = f"https://www.thailovefood.com/menu_info/{id}"
+#     url2 = f"http://yogiyofind.ddns.net/getMenus?id={id}"
+#     try:
+#         response = requests.get(url,headers=header,verify=False,timeout=2)
+#     except:
+#         response = requests.get(url2,headers=header,verify=False)
+#     Get_json = response.json()
 
 #     return Get_json
+
+def get_Menu(id):
+    header = {   
+        
+        'Cookie': 'PHPSESSID=rh3a3euioieoau71nd72s2q76u',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Safari Line/13.2.1 LIFF',
+        'Accept-Language': 'ko-KR,ko;q=0.9',
+        
+        }
+
+    url = f"https://youduay.com/datacenter/getajaxfood.php?type=6&sid={id}"
+
+    scraper = cloudscraper.create_scraper()
+    response = scraper.get(url, headers=header)
+    Get_json = response.json()
+
+    return Get_json
 
 def getItemReviews(id, page, count, menu_id):
     header = {"x-apikey": 'iphoneap',
